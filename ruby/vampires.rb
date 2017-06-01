ff410d7bdb5e42f9f15f4f51824c5c6d8bcba608
Jennifer Shopsin <jshopsin@yahoo.com>
2017-06-01 00:39:59 -0700

    Answer if employee is a vampire or not

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,6 +3,12 @@
 puts "What is your name?"
 name = gets.chomp
 
+if name == "Drake Cula" || name == "Tu Fang"
+  vamp_name = true
+else
+  vamp_name = false
+end
+
 # Check to see if potential employee knows the correct year correlated with the age they give. This is to try to trick the vampire, who is likely several hundreds of years old. If an employee gives an age and a year of birth that don’t line up mathematically, that employee might be a vampire.
 
 current_year = 2017
@@ -51,4 +57,28 @@ else insurance == "no" || "n"
   weres_mortal == false
 end
 
+# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
 
+if age_correct == true && (weres_like_garlic == true || weres_mortal == true)
+  puts "Probably not a vampire."
+
+  # If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+
+elsif age_correct == false && (weres_like_garlic == false || weres_mortal == false)
+  puts "Probably a vampire."
+
+  # If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+
+elsif age_correct == false && weres_like_garlic == false && weres_mortal == false
+  puts "Almost certainly a vampire."
+
+  # Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+
+elsif vamp_name == true
+  puts "Definitely a vampire."
+
+  # Otherwise, print “Results inconclusive.”
+
+else
+  puts "Results inconclusive."
+end
