0976037cfea1358b79a8b1b958d3070589dd9ab1
Jennifer Shopsin <jshopsin@yahoo.com>
2017-05-31 22:59:40 -0700

    Add employee trick questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "#{name}, how old are you? What year were you born?"
+age_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
