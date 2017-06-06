8da5ae3282042ffb8bec5d1899e3f71f224d47f6
Jennifer Shopsin <jshopsin@yahoo.com>
2017-06-05 22:15:44 -0700

    Add psuedocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+# Client's name
+# Client's age
+# Number of Children
+# Renovation or New Construction
+# Project Type
+# Project Address
+# Square Footage of Project
+# Decor Theme
+# Budget
+# First-time Client
