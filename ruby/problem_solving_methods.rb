efe5341db5e14d466fcdf2c00c250d0927016e5e
Jennifer Shopsin <jshopsin@yahoo.com>
2017-06-09 15:41:32 -0700

    Add fib nums method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,4 +1,4 @@
-# PSUEDOCODE:
+# RELEASE 0 - Implement a Simple Search:
 
 # define array
 # create a method that searches the array for a value and returns the index it is located or nil
@@ -27,3 +27,34 @@ end
 
 p search_array(arr, 1)
 p search_array(arr, 24)
+
+# RELEASE 1 - Calculate Fibonacci Numbers
+
+# method 1:
+
+def fib(x)
+  fib_nums = [0,1]
+  number = x - 2
+  number.times do
+    fib_nums << fib_nums[-2] + fib_nums[-1]
+  end
+  p fib_nums
+end
+
+fib(6)
+fib(100)
+
+# method 2:
+
+def fibonacci(n)
+  index = 0
+  fib_array = [0,1]
+  until fib_array.length == n
+    continued_array = fib_array[index] +fib_array[index + 1]
+    fib_array << continued_array
+    index += 1
+  end
+  p fib_array
+end
+
+fibonacci(6)
