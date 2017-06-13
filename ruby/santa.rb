0313c38c978c17f95bbaecbebfbec7f6d950df90
Jennifer Shopsin <jshopsin@yahoo.com>
2017-06-12 22:11:59 -0700

    Add attributes and driver code with an empty array to store new santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,12 @@
 class Santa
 
-  def initialize
-    puts "Initializing Santa instance ..."
+  def initialize(name, gender, ethnicity)
+    puts "Initializing Santa instance ... #{name} has been added."
+    @name = name
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak()
@@ -14,7 +19,18 @@ class Santa
 
 end
 
-santa = Santa.new
+santas = []
 
-puts santa.speak
-puts santa.eat_milk_and_cookies("Chocolate Chip")
+santas << Santa.new("Shirley","female", "N/A")
+santas << Santa.new("Hampton","male", "White")
+santas << Santa.new("Rudolph","male", "Dog")
+santas << Santa.new("Lauren","bigender", "Japanese")
+santas << Santa.new("Hugh","male", "English")
+santas << Santa.new("Sarah","female", "Elf")
+
+p santas
+
+# santa = Santa.new
+
+# puts santa.speak
+# puts santa.eat_milk_and_cookies("Chocolate Chip")
