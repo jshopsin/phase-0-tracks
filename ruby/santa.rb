4c8c83a46dc41c7c87b716c266787dad42cfac4c
Jennifer Shopsin <jshopsin@yahoo.com>
2017-06-12 22:37:49 -0700

    Add celebrate birthday and get mad at methods to Santa class with driver code to test it

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,18 +16,41 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    if @reindeer_ranking.include?(reindeer)
+      @reindeer_ranking.delete(reindeer)
+      @reindeer_ranking << reindeer
+    end
+  end
+
   # setter methods
   def gender= (new_gender)
     @gender = new_gender
   end
 
   # getter methods
+  def name
+    @name
+  end
+
+  def gender
+    @gender
+  end
+
   def age
     @age
   end
 
   def ethnicity
     @ethnicity
+  end
+
+  def reindeer_ranking
+    @reindeer_ranking
   end
 end
 
@@ -40,7 +63,18 @@ santas << Santa.new("Lauren","bigender", "Japanese")
 santas << Santa.new("Hugh","male", "English")
 santas << Santa.new("Sarah","female", "Elf")
 
-p santas
+santas[1].gender = "jock male"
+santas[1].celebrate_birthday
+
+puts "#{santas[1].name} is a #{santas[1].gender} who is #{santas[1].age} years old."
+
+santas[2].get_mad_at("Rudolph")
+puts "#{santas[2].name}'s reindeer ranking is #{santas[2].reindeer_ranking}."
+
+santas[3].get_mad_at("Hershel")
+puts "#{santas[3].name}'s reindeer ranking is #{santas[3].reindeer_ranking}."
+
+# p santas
 
 # santa = Santa.new
 
