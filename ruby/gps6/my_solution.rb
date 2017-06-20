63e353109aa7882f900d46d392f3c09caff471b8
Jennifer Shopsin <jshopsin@yahoo.com>
2017-06-19 18:47:41 -0700

    Add UI report to print each state from STATE_DATA

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,26 +4,29 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# require allows you to link existing modules to have access to the methods within
+# require_relative allows you to link local files to have access to the data within
+
 require_relative 'state_data'
 
 class VirusPredictor
-
+  # stores given data per state_of_origin into each instance that is created
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  # wrapping method that calls the predicted deaths and speed of spread methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  # calculates the number of deaths based on a conditional of the population density
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +44,8 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  # gives a specific value to speed of the spread of a virus based on the population density of that state (instance)
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -64,6 +68,15 @@ class VirusPredictor
 
 end
 
+# USER INTERFACE
+
+STATE_DATA.each do |state, data|
+  state = VirusPredictor.new(state, data[:population_density], data[:population])
+  state.virus_effects
+end
+
+
+=begin
 #=======================================================================
 
 # DRIVER CODE
@@ -85,3 +98,5 @@ alaska.virus_effects
 
 #=======================================================================
 # Reflection Section
+
+=end
