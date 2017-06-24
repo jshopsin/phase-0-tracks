962796aee10f471783407d17a5334d3f2fe5aa44
Jennifer Shopsin <jshopsin@yahoo.com>
2017-06-23 18:54:27 -0700

    Add includePair function with driver code to test all conditions

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,6 @@
-// Release 0 - Find the Longest Phrase
+// PSUEDOCODE
 
-// Psuedocode
+// RELEASE 0 -  Find the Longest Phrase
 // create a function that takes an array and returns longest string
 // input: array
 // steps:
@@ -8,7 +8,20 @@
   // FOR each item in the array, change the value of longestString to equal the string in the array IF it's length is larger than the current longestString
 // output: longest string
 
-words = ["hello", "start", "long", "short", "end", "longest"]
+// RELEASE 1 - Find a Key-Value Match
+// create a function that takes two objects and see if they share a key-value pair
+// input: objectA, objectB
+// steps:
+  // FOR each key in ObjectA:
+    // compare key to EACH key in Object B
+    // IF the key is the same, IF the value is also the same then return true
+    // ELSE return false
+// output: boolean
+
+
+// RELEASE 0 - BUSINESS LOGIC
+words1 = ["hello", "starting", "long", "short", "ending"]
+words2 = ["long", "longer", "longest"]
 
 var longestWord = function(array) {
   longestString = "";
@@ -19,4 +32,43 @@ var longestWord = function(array) {
   console.log(longestString);
 };
 
-longestWord(words)
+// RELEASE 1 - BUSINESS LOGIC
+horse1 = {name: "Windy", age: 2, color: "white"};
+horse2 = {name: "Winston", age: 6, color: "brown"};
+bird1 = {name: "Windy", age: 6, eyeColor: "blue"};
+bird2 = {name: "Harley", age: 10, eyeColor: "blue"};
+
+var includePair = function(objA, objB) {
+  include = false;
+  aKeys = [];
+  bKeys = [];
+  for (var key in objA) { aKeys.push(objA[key]) };
+  for (var key in objB) { bKeys.push(objB[key]) };
+  for (i in aKeys) {
+    while (include !== true) {
+      if (aKeys[i] === bKeys[i]) {
+        if (objA[i] === objB[i]) {
+          console.log("The objects share a key-value pair!");
+          include = true;
+        } else { include = false };
+      };
+      if(include === false) {
+        break;
+      };
+    };
+  };
+  if (include === false) { console.log("The objects do not share a key-value pair.") };
+};
+
+// RELEASE 0 - DRIVER CODE
+longestWord(words1);
+longestWord(words2);
+
+// RELEASE 1 - DRIVER CODE
+
+includePair(horse1, bird1); // true
+includePair(horse1, bird2); // false
+includePair(horse2, bird1); // true
+includePair(horse2, bird2); // false
+includePair(horse1, horse2); // false
+includePair(bird1, bird2); // true
