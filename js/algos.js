b80548c0ceaecbd344cd5cc0a6218a6914534ea2
Jennifer Shopsin <jshopsin@yahoo.com>
2017-06-24 01:05:50 -0700

    Refactor code for randomString generator method

--- js/algos.js
+++ js/algos.js
@@ -20,10 +20,26 @@
 
 // RELEASE 2 - Generate Random Test Data
 // create a function that outputs a given number of strings at a random length between 1 and 10
-// input: integer
 // steps:
-  //
-// output: array
+  // define an array of the alphabet
+  // define an empty string to later push random letters into
+  // define an empty array to later push random strings into
+
+  // create a function that outputs a random string:
+  // no input
+  // steps:
+    // FOR each index of a string at a random length between 1 and 10:
+      // push a random letter from the alphabet array into the empty string
+    // return the string to be used in another function
+  // output: string
+
+  // create a function creates a given number of random strings
+  // input: number
+  // steps:
+    // FOR the given number of strings:
+      // push a random outputted string into the array of strings
+    // print the strings array on the console
+  // output: array
 
 
 // RELEASE 0 - BUSINESS LOGIC
@@ -75,21 +91,22 @@ var includePair = function(objA, objB) {
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // var alphapet = ["a".."z"]
 var randomString = "";
-var strings = [];
 
 var outputString = function() {
-  for (var i = 0; i < Math.round(Math.random() * (10)); i++) {
-    randomString += alphabet[Math.round(Math.random() * alphabet.length)];
+  var randomString = "";
+  for (var i = 0; i < Math.floor(Math.random() * (10) + 1); i++) {
+    randomString += alphabet[Math.floor(Math.random() * (alphabet.length))];
   };
+  return randomString;
 };
 
 var randomStrings = function(num) {
+  var strings = [];
   for (var i = 0; i < num; i++) {
     strings.push(outputString());
   };
   console.log(strings);
 };
-
 
 
 // RELEASE 0 - DRIVER CODE
@@ -107,5 +124,5 @@ includePair(bird1, bird2); // true
 // RELEASE 2 - DRIVER CODE
 randomStrings(2);
 randomStrings(5);
-randomStrings(10);
+randomStrings(8);
 randomStrings(3);
