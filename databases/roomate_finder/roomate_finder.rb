fdcdbe08dc4a679561a281ad4cca935c32afd918
Jennifer Shopsin <jshopsin@yahoo.com>
2017-07-08 18:17:02 -0700

    Add ruby file with psuedocode

---
+++ databases/roomate_finder/roomate_finder.rb
@@ -0,0 +1,31 @@
+# ROOMATE FINDER
+# Going to a convention and want to share a hotel room with someone to save some money...
+# Use roomate finder to create a profile and find matches who are attending the same convention as you
+
+# require gems
+# create SQLite3 database
+# create table
+  # values:
+    # id integer primary key
+    # name
+    # email
+    # convention_name
+    # convention_location
+    # home_state
+    # home_city
+    # age
+    # smoker
+
+# add lots of fake people for testing
+
+# BUSINESS LOGIC
+# create a method that finds matches
+
+
+# create a method to send requested match
+  # if interested then respond yes
+  # else request is ignored and noted that match not desired
+
+# UI
+# allow user to create a profile
+# allow user to find a match
